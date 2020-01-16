c2d366b46e02cbebb5468f4fabc681c7d614dd0f
sbcordaro <59941087+sbcordaro@users.noreply.github.com>
2020-01-15 18:49:27 -0600

    This is a POJO simulating a lion

---
+++ src/AnimalNoises.java
@@ -0,0 +1,13 @@
+import model.*;
+
+public class AnimalNoise {
+	//add your animal class to the model package
+	//only push this file and the animal class you created
+
+	public static void main(String[] args) {
+		Lion leo = new Lion();
+		System.out.println(leo.speak());
+		
+	}
+
+}
---
+++ src/model/Lion.java
@@ -0,0 +1,49 @@
+package model;
+
+
+public class Lion {
+	private String name;
+	private String color;
+	private int age;
+
+	public Lion() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Lion(String name, String color, int age) {
+		super();
+		this.name = name;
+		this.color = color;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String speak() {
+		return "Roar";
+	}
+
+}
