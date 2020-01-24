b6836cfa5516447aa7f6263a89400b53974ae677
sunnychng <60238115+sunnychng@users.noreply.github.com>
2020-01-23 19:08:49 -0600

    Add files via upload

--- src/AnimalNoise.java
+++ src/AnimalNoise.java
@@ -31,13 +31,9 @@ public class AnimalNoise {
 		
 		Mouse jerry = new Mouse("Jerry", "Brown", 2);
 		System.out.println(jerry.speak());
-    
-    Parrot polly = new Parrot(false, false, new String[]{"polly wanna cracker"});
-		System.out.println(polly.speak());
 		
-		Frog kermit = new Frog("Kermit", "Green", 64);
-		System.out.println(kermit.speak());
-
+		Rooster leghorn = new Rooster("Foghorn Leghorn", "White", 4);
+		System.out.println(leghorn.speak());
 	}
 
 }
